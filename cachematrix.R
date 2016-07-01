fea72a2f1c6043e0957fc8a8102c31d057213216
vepferreira <vepferreira@gmail.com>
2016-07-01 00:13:20 -0300

    Revert "Completed Functions"
    This reverts commit c02c0cb1496e2754accd5a001d8ad755e33fe0fa.

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,15 @@
-## makeCacheMatrix defines getter and setter functions for the matrix 'x' and its inverse.
-## cacheSolve first checks if the cacheMatrix already has a stored inverse. If it does not,
-## it calculates and stores the inverse of 'x'.
+## Put comments here that give an overall description of what your
+## functions do
 
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-      inv <- NULL
-      set <- function(y) {
-            x <<- y
-            inv <<- NULL
-      }
-      get <- function() x
-      setinv <- function(inverse) inv <<- inverse
-      getinv <- function() inv
-      list(set = set, get = get, setinv = setinv, getinv = getinv)
+
 }
 
 
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-      inv <- x$getinv()
-      if(!is.null(inv)) { ## If the inverse of 'x' has already been calculated, return cached data
-            message("getting cached data")
-            return(inv)
-      }
-      ## Else, calculate and store the inverse of 'x' in cache
-      data <- x$get()
-      inv <- solve(data)
-      x$setinv(inv)
-      inv
 }
